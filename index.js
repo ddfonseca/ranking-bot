ee6879724458e0309cef6f45cd974e657250d5e0
David <olfdavidgmail.com>
2021-09-09 18:44:59 -0300

    index updated

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import bot from './utils/Bot'
 
 const app = express()
 app.set('port', process.env.PORT || 5000)
-// app.get('/', (req, resp) => {
-//     // const result = 'App is running'
-//     resp.send(result)
-// }).listen(app.get('port'), () => {
-//     // console.log('App is running, server is listenning on port', app.get('port'))
-// })
+app.get('/', (req, resp) => {
+    const result = 'App is running'
+    resp.send(result)
+}).listen(app.get('port'), () => {
+    console.log('App is running, server is listenning on port', app.get('port'))
+})
 
 bot.on('text', (msg) => {
     const userId = msg.from.id
